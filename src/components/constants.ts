7aa822507f0cdd58fce826d3150f6ee8fe528623
MohaK <mohakhalili@outlook.com>
2024-09-06 14:45:16 +0200

    rebuilding deployment frontend

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://bibliotekssystembackend-1.onrender.com"
+    ? "https://bibliotekssystembackend-2.onrender.com"
     : "http://localhost:5588";
